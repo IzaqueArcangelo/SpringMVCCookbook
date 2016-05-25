6f4bc346b0f327a0d7c2942515376352efa92d58
Izaque <izk_arcangelo@hotmail.com>
2016-05-25 00:42:11 -0300

    Criação de um bean para o teste da saída de uma String com as configurações do servidor.

--- cloudstreetmarket-webapp/src/main/java/edu/zipcloud/cloudstreetmarket/portal/controllers/InfoTagController.java
+++ cloudstreetmarket-webapp/src/main/java/edu/zipcloud/cloudstreetmarket/portal/controllers/InfoTagController.java
@@ -1,20 +1,45 @@
 package edu.zipcloud.cloudstreetmarket.portal.controllers;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.StringJoiner;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.WebApplicationContext;
 
 /**
- * pg 47 do livro.
+ * Controlador padrão da aplicação
  * 
  */
 @Controller
 @RequestMapping("/info")
 public class InfoTagController {
 	
+	@Autowired
+	private WebApplicationContext webAppContext;
+	private final static LocalDateTime startDateTime = LocalDateTime.now();
+	private final static DateTimeFormatter DT_FORMATTER = DateTimeFormatter.ofPattern("EEE, d MMM yyyy h:mm a");
+	
+	
 	@RequestMapping("/helloHandler")
 	@ResponseBody
-	public String helloController(){
+	public String helloController() {
 		return "Olá Mundo";
+	}
+
+
+	@RequestMapping("/server")
+	@ResponseBody
+	public String infoTagServer() {
+		return new StringJoiner("<br>")
+				.add("-------------------------------------")
+				.add(" Servidor: " + webAppContext.getServletContext().getServerInfo())
+				.add(" Data: " + startDateTime.format(DT_FORMATTER))
+				//webAppVersion é um bean criado no dispatcher-context.xml que retorna uma String.
+				.add(" Versão: " + webAppContext.getBean("webAppVersion"))
+				.add("--------------------------------------").toString();
 	}
 }
