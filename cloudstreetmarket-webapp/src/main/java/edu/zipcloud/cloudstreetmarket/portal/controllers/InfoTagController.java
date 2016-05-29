39e7af42d90ceb81c6e5d36566914d7eabcd5d03
Izaque <izk_arcangelo@hotmail.com>
2016-05-29 00:00:44 -0300

    Lição do livro até a página 59/78 : Aprendendo sobre a anotação request mapping e a utilizar o mapeamento de urls no browser.

--- cloudstreetmarket-webapp/src/main/java/edu/zipcloud/cloudstreetmarket/portal/controllers/InfoTagController.java
+++ cloudstreetmarket-webapp/src/main/java/edu/zipcloud/cloudstreetmarket/portal/controllers/InfoTagController.java
@@ -2,13 +2,19 @@ package edu.zipcloud.cloudstreetmarket.portal.controllers;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.StringJoiner;
+
+import javax.servlet.ServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.portlet.ModelAndView;
 
 /**
  * Controlador padrão da aplicação
@@ -17,29 +23,48 @@ import org.springframework.web.context.WebApplicationContext;
 @Controller
 @RequestMapping("/info")
 public class InfoTagController {
-	
+
 	@Autowired
 	private WebApplicationContext webAppContext;
 	private final static LocalDateTime startDateTime = LocalDateTime.now();
-	private final static DateTimeFormatter DT_FORMATTER = DateTimeFormatter.ofPattern("EEE, d MMM yyyy h:mm a");
-	
-	
+	private final static DateTimeFormatter DT_FORMATTER = DateTimeFormatter
+			.ofPattern("EEE, d MMM yyyy h:mm a");
+
 	@RequestMapping("/helloHandler")
 	@ResponseBody
 	public String helloController() {
 		return "Olá Mundo";
 	}
 
-
 	@RequestMapping("/server")
 	@ResponseBody
 	public String infoTagServer() {
 		return new StringJoiner("<br>")
 				.add("-------------------------------------")
-				.add(" Servidor: " + webAppContext.getServletContext().getServerInfo())
+				.add(" Servidor: "
+						+ webAppContext.getServletContext().getServerInfo())
 				.add(" Data: " + startDateTime.format(DT_FORMATTER))
-				//webAppVersion é um bean criado no dispatcher-context.xml que retorna uma String.
+				// webAppVersion é um bean criado no dispatcher-context.xml que
+				// retorna uma String.
 				.add(" Versão: " + webAppContext.getBean("webAppVersion"))
 				.add("--------------------------------------").toString();
 	}
+
+//	@RequestMapping(value = "/index")
+//	public ModelAndView getRequestExample(ServletRequest request) {
+//		ModelAndView mav = new ModelAndView();
+//		mav.setViewName("index");
+//		mav.addObject("variable1", new ArrayList<String>());
+//		return mav;
+//	}
+//
+//	@RequestMapping(value = "/index")
+//	public ModelAndView getRequestExample(@RequestParam("exP1") String exP1) {
+//		ModelAndView mav = new ModelAndView();
+//		mav.setViewName("index");
+//		mav.addObject("exP1", exP1);
+//		return mav;
+//	}
+
+	
 }
